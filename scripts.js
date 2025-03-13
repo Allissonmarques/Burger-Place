e0f086798de037a9ba9b8dbae38acd3e8fcc646d
Allissonmarques <allissonmarques00@gmail.com>
2025-03-13 16:36:50 -0300

    Projeto finalizado.

--- scripts.js
+++ scripts.js
@@ -2,7 +2,13 @@ const menu = document.querySelector('ul')
 const showAll = document.querySelector('.show-all')
 const map = document.querySelector('.map')
 const addItAllUp = document.querySelector('.add-it-all-up')
+const filterVeganOnly = document.querySelector('.filter-vegan-only')
 let myLi = ''
+
+function formatValue(value){
+    const formattedValue = value.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+    return formattedValue
+}
 
 function buttonShowAll(optionsArray){
     myLi = ''
@@ -12,7 +18,7 @@ function buttonShowAll(optionsArray){
             `<li>
                     <img src=${element.src} id="jpeg">
                     <p class="burger-name">${element.name}</p>
-                    <p class="value">R$ ${element.price}</p>
+                    <p class="value"> ${formatValue(element.price)}</p>
         </li>
         `
     })
@@ -38,11 +44,21 @@ function sumButton(){
 
     menu.innerHTML =`
         <li>
-                    <p class="value"> O valor total da soma de todos os itens é R$ ${totalValue}</p>
+                    <p class="value"> O valor total da soma de todos os itens é ${formatValue(totalValue)}</p>
         </li>
     `
 }
 
+function veganFilterButton(){
+
+    const veganOptions = menuOptions.filter((options) => options.vegan)
+
+    buttonShowAll(veganOptions)
+}
+
+
+
 showAll.addEventListener('click', () => buttonShowAll(menuOptions) )
 map.addEventListener('click', buttonMap)
 addItAllUp.addEventListener('click', sumButton)
+filterVeganOnly.addEventListener('click', veganFilterButton)
